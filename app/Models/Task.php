0614ba3a89b78abac035edd0d7ec5ad5e1c01f4d
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2024-06-12 15:37:02 +0900

    以下の内容を実施
    - カテゴリテーブルの作成
    - 中間テーブル（task_categories）の作成
    - リレーション対応（タスクとユーザー、タスクとカテゴリ）

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+use App\Models\Category;
+
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content'];
+
+    // 一対多の関係（ユーザーとタスク）
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    
+    // 多対多の関係（カテゴリとタスク）
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
