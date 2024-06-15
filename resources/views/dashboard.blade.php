c04d406c487bb1f1a906829b1cbe5528f87b3001
Takeshi Kobayashi <t.kobayashi3927@gmail.com>
2024-06-15 16:41:52 +0900

    ルートの設定・コントローラの対応

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Index of tasks') }}
         </h2>
+        <button type="button" class="text-white m-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onclick="location.href='{{ route('tasks.create') }}' "
+        >New Task</button>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    
                 </div>
             </div>
         </div>
